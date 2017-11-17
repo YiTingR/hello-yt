30285e11d22b92b24613bc47a925c58e9f203d80
YitingRen <ryt@YitingRendeiMac.local>
2017-11-17 12:24:07 +0800

    Initial Commit

---
+++ MyApp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp1
+//
+//  Created by YitingRen on 2017/11/17.
+//  Copyright © 2017年 YitingRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyApp1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp1
+//
+//  Created by YitingRen on 2017/11/17.
+//  Copyright © 2017年 YitingRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
