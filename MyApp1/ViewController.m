8d7a42665c4cd7d31fc9c856ea8e9db8777bafd1
YiTingR <619118752@qq.com>
2017-11-28 16:53:58 +0800

    第一次更新

--- MyApp1/ViewController.m
+++ MyApp1/ViewController.m
@@ -19,6 +19,24 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)openAnotherApp:(id)sender {
+    //创建一个url，这个url就是WXApp的url，记得加上：//
+    NSURL *url = [NSURL URLWithString:@"weixin://"];
+    
+    //打开url
+//    [[UIApplication sharedApplication] openURL:url];
+
+    
+    //先判断是否能打开该url
+    if ([[UIApplication sharedApplication] canOpenURL:url]) {
+        //打开url
+        [[UIApplication sharedApplication] openURL:url];
+    }else {
+        //给个提示或者做点别的事情
+        NSLog(@"U四不四洒，没安装WXApp，怎么打开啊！");
+    }
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
